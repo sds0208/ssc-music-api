fc3ea93a85b7be23912874a1f4ef5b1643cc75be
Sarah Santa Cruz <sarahsantacruz@Sarahs-Mac-mini.lan>
2024-05-13 19:07:07 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const express = require('express');
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(express.json());
+
+const corsOptions = {
+    origin: ['https://sscmusic.netlify.app/', 'https://sscmusic.netlify.app/'],
+    optionsSuccessStatus: 200,
+  };
+ 
+app.use(cors(corsOptions));
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log("Server Listening on PORT:", PORT);
+});
+
+app.get('/', (request, response) => {
+    let json;
+    try {
+        json = fs.readFileSync('./music-samples.json', { encoding: 'utf8', flag: 'r' });
+    } catch (err) {
+        response.send(err);
+        throw err;
+    }
+    
+    response.send(json);
+});
