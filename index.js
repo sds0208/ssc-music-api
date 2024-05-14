66260a1e26d9a85cf59d625ee9a0c2ccc21241d5
Sarah Santa Cruz <sarahsantacruz@Sarahs-Mac-mini.lan>
2024-05-14 07:57:00 -0500

    updated cors origin urls again

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(express.json());
 
 const corsOptions = {
-    origin: ['https://sscmusic.netlify.app/', 'https://sscmusic.net/'],
+    origin: ['https://sscmusic.netlify.app', 'https://sscmusic.net'],
     optionsSuccessStatus: 200,
   };
  
